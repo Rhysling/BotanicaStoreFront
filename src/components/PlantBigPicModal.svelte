5e5f891c1b15380d4082b179d4f54dee801d72cf
Rhysling <rkummer@polson.com>
2022-06-05 10:56:00 -0700

    Simplify keydown.

--- src/components/PlantBigPicModal.svelte
+++ src/components/PlantBigPicModal.svelte
@@ -1,124 +1,115 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
-  import Modal from "./Modal.svelte";
+	import Modal from "./Modal.svelte";
 
-  export let bigPicPaths: PicIdPath[] = [];
-  export let isShowModal: boolean = false;
+	export let bigPicPaths: PicIdPath[] = [];
+	export let isShowModal: boolean = false;
 
-  let last: number;
-  let ix = 0;
-  let hasMultiple: boolean;
+	let last: number;
+	let ix = 0;
+	let hasMultiple: boolean;
 
-  $: {
-    last = bigPicPaths.length - 1;
-    ix = 0;
-    hasMultiple = last > 0;
-  }
+	$: {
+		last = bigPicPaths.length - 1;
+		ix = 0;
+		hasMultiple = last > 0;
+	}
 
-  const changePic = (n: number) => {
-    let i = ix + n;
+	const changePic = (n: number) => {
+		let i = ix + n;
 
-    if (i < 0) {
-      ix = last;
-      return;
-    }
+		if (i < 0) {
+			ix = last;
+			return;
+		}
 
-    if (i > last) {
-      ix = 0;
-      return;
-    }
+		if (i > last) {
+			ix = 0;
+			return;
+		}
 
-    ix = i;
-  };
+		ix = i;
+	};
 
-  const moveByArrow = (e: KeyboardEvent) => {
-    if (!isShowModal || !hasMultiple) return;
+	const moveByArrow = (e: KeyboardEvent) => {
+		if (!isShowModal || !hasMultiple) return;
 
-    if (e.key == "ArrowRight") {
-      changePic(1);
-      return;
-    }
+		if (e.key == "ArrowRight") {
+			changePic(1);
+			return;
+		}
 
-    if (e.key == "ArrowLeft") {
-      changePic(-1);
-    }
-  };
-
-  onMount(() => {
-    document.addEventListener("keydown", moveByArrow);
-  });
-
-  onDestroy(() => {
-    document.removeEventListener("keydown", moveByArrow);
-});
+		if (e.key == "ArrowLeft") {
+			changePic(-1);
+		}
+	};
 
 </script>
 
+<svelte:window on:keydown={(e) => moveByArrow(e)} />
 <Modal {isShowModal} on:setmodal>
-  {#if isShowModal}
-  <div class="frame" on:click={(e) => e.stopPropagation()}>
-    {#if hasMultiple}
-      <a href="/" class="arrow left" on:click|preventDefault|stopPropagation={() => changePic(-1)}><i class="fas fa-angle-left"></i></a>
-    {/if}
-    <img src="{bigPicPaths[ix].path}" alt="Botanica plant" />
-    {#if hasMultiple}
-      <a href="/" class="arrow right" on:click|preventDefault|stopPropagation={() => changePic(1)}><i class="fas fa-angle-right"></i></a>
-    {/if}
-  </div>
-  {/if}
+	{#if isShowModal}
+	<div class="frame" on:click={(e) => e.stopPropagation()}>
+		{#if hasMultiple}
+			<a href="/" class="arrow left" on:click|preventDefault|stopPropagation={() => changePic(-1)}><i class="fas fa-angle-left"></i></a>
+		{/if}
+		<img src="{bigPicPaths[ix].path}" alt="Botanica plant" />
+		{#if hasMultiple}
+			<a href="/" class="arrow right" on:click|preventDefault|stopPropagation={() => changePic(1)}><i class="fas fa-angle-right"></i></a>
+		{/if}
+	</div>
+	{/if}
 </Modal>
 
 <style lang="scss">
-  @import "../styles/_custom-variables.scss";
+	@import "../styles/_custom-variables.scss";
 
-  .frame {
-    position: relative;
-    height: 90vh;
-    width: 100%;
-    margin-top: 5vh;
-  }
+	.frame {
+		position: relative;
+		height: 90vh;
+		width: 100%;
+		margin-top: 5vh;
+	}
 
-  img {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    max-width: 95vw;
-    max-height: 90vh;
-  }
+	img {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		max-width: 95vw;
+		max-height: 90vh;
+	}
 
-  .arrow {
-    display: block;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    font-size: 5rem;
-    z-index: 2;
+	.arrow {
+		display: block;
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		font-size: 5rem;
+		z-index: 2;
 
-    &.left {
-      left: 2rem;
-    }
+		&.left {
+			left: 2rem;
+		}
 
-    &.right {
-      right: 2rem;
-    }
-  }
+		&.right {
+			right: 2rem;
+		}
+	}
 
-  i {
-    color: rgba(255, 255, 255, 0.3);
-  }
+	i {
+		color: rgba(255, 255, 255, 0.3);
+	}
 
-  a {
-    text-decoration: none;
+	a {
+		text-decoration: none;
 
-    i {
-      color: rgba(255, 255, 255, 0.7);
+		i {
+			color: rgba(255, 255, 255, 0.7);
 
-      &:hover {
-        color: rgba(255, 255, 255, 0.9);
-      }
-    }
-  }
-
+			&:hover {
+				color: rgba(255, 255, 255, 0.9);
+			}
+		}
+	}
 
 </style>
---
+++ src/components/TpTesting.svelte
@@ -0,0 +1,35 @@
+<script lang="js">
+	let foo = [1, 2, 3];
+
+	const arrow = (e) => {
+		switch(e.key) {
+			case "ArrowRight":
+				alert("Right Arrow");
+				break;
+			case "ArrowLeft":
+				alert("Left Arrow");
+				break;
+			default:
+				// noop
+		}
+
+	};
+
+</script>
+
+<svelte:window on:keydown={(e) => arrow(e)} />
+
+{#each foo as item}
+	<div>
+		Foo says: {item}
+	</div>
+{/each}
+
+<div class="moog">Bar</div>
+
+<style lang="scss">
+	.moog {
+		color: aquamarine;
+		font-weight: bold;
+	}
+</style>
